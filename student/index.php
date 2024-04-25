19005e60a896950b21b64baef108739b8ca3f446
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-04-25 18:40:43 +0530

    Add files via upload

---
+++ student/index.php
@@ -0,0 +1,53 @@
+<?php include "includes/header.php"; ?>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+		<?php include "includes/left-nav.php"; ?>
+
+        <?php include "includes/top-nav.php"; ?>
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Dashboard </h3>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-15 col-sm-15 col-xs-15">
+                <div class="x_panel">
+                  <div class="x_title">
+                        <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Settings 1</a>
+                          </li>
+                          <li><a href="#">Settings 2</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <img src="../assets/images/dashboard2.png" height="575" width="1250" alt="">
+                  </div>
+                </div>
+              </div>
+            </div> 
+                   <!-- /page content -->
+
+        <?php include "includes/footer-nav.php"; ?>
+      </div>
+    </div>
+  </body>
+    <?php include "includes/footer.php"; ?>
