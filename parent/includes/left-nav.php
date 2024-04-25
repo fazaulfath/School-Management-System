7294862e95a9e508350f02bd48ff28cb2d277b26
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-04-25 18:37:22 +0530

    Add files via upload

---
+++ parent/includes/left-nav.php
@@ -0,0 +1,61 @@
+<div class="col-md-3 left_col">
+	<div class="left_col scroll-view">
+		<div class="navbar nav_title" style="border: 0; background color= navy">
+		  <a href="index.php" class="site_title"><i class="fa fa-graduation-cap"></i> <span>School Management</span></a>
+		</div>
+		<div class="clearfix"></div>
+		
+		<!-- sidebar menu -->
+		<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+		  <div class="menu_section">
+			<ul class="nav side-menu">
+			  <li><a href="index.php"><i class="fa fa-home"></i> Dashboard </a></li>
+			  <li><a href="teacherlist.php"><i class="fa fa-user"></i> Teacher</a></li> 
+			  <li><a><i class="fa fa-bars"></i> Academic Syllabus<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="syllabuslist.php">Student1</a></li>
+					<li><a href="ayllabuslist.php">Student22</a></li>
+				</ul>
+			  </li>
+			  <li><a><i class="fa fa-clock-o"></i> Class Routine<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="class1.php">Student1</a></li>
+					<li><a href="class2.php">Student22</a></li>
+				</ul>
+			  </li>
+			  <li><a><i class="fa fa-th-large"></i> Exam Marks<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="markslist.php">Student1</a></li>
+					<li><a href="markslist.php">Student22</a></li>
+				</ul>
+			  </li>
+			  <li><a href="librarylist.php"><i class="fa fa-th-list"></i> Library</a>
+			  <li><a href="transportlist.php"><i class="fa fa-bus"></i> Transport</a>  
+			  <li><a><i class="fa fa-circle"></i> Noticeboard</a> 
+			  <li><a><i class="fa fa-envelope"></i> Message</a>
+			  <li><a><i class="fa fa-laptop"></i> Account</a></li>
+			</ul>
+		  </div>
+		  
+
+		</div>
+		<!-- /sidebar menu -->
+
+		<!-- /menu footer buttons -->
+		<!--div class="sidebar-footer hidden-small">
+		  <a data-toggle="tooltip" data-placement="top" title="Settings">
+			<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+			<span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Lock">
+			<span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+			<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+		  </a>
+		</div-->
+		<!-- /menu footer buttons -->
+	  </div>
+</div>
