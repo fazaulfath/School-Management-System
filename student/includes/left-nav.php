5b2412b52c298ba06ef5789f5f1b07ec585c4ae8
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-04-25 18:40:19 +0530

    update includes

---
+++ student/includes/left-nav.php
@@ -0,0 +1,48 @@
+<div class="col-md-3 left_col">
+	<div class="left_col scroll-view">
+		<div class="navbar nav_title" style="border: 0; background color= navy">
+		  <a href="index.php" class="site_title"><i class="fa fa-graduation-cap"></i> <span>School Management</span></a>
+		</div>
+		<div class="clearfix"></div>
+
+		<!-- sidebar menu -->
+		<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+		  <div class="menu_section">
+			<ul class="nav side-menu">
+			  <li><a href="index.php"><i class="fa fa-home"></i> Dashboard </a></li>
+			  <li><a href="teacherlist.php"><i class="fa fa-user"></i> Teacher</a></li> 
+			  <li><a href="Subjectlist.php"><i class="fa fa-book"></i> Subject</a></li>
+			  <li><a href="routine.php"><i class="fa fa-clock-o"></i> Class Routine</a></li>
+			  <li><a href="studymaterial.php"><i class="fa fa-clone"></i> Study Material</a></li>
+			  <li><a href="syllabuslist.php"><i class="fa fa-bars"></i> Academic Syllabus</a></li>
+			  <li><a href="markslist.php"><i class="fa fa-th-large"></i> Exam Marks</a></li>
+			  <li><a href="librarylist.php"><i class="fa fa-th-list"></i> Library</a>
+			  <li><a href="transportlist.php"><i class="fa fa-bus"></i> Transport</a>  
+			  <li><a><i class="fa fa-circle"></i> Noticeboard</a> 
+			  <li><a><i class="fa fa-envelope"></i> Message</a>
+			  <li><a><i class="fa fa-laptop"></i> Account</a></li>
+			</ul>
+		  </div>
+		  
+
+		</div>
+		<!-- /sidebar menu -->
+
+		<!-- /menu footer buttons -->
+		<!--div class="sidebar-footer hidden-small">
+		  <a data-toggle="tooltip" data-placement="top" title="Settings">
+			<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+			<span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Lock">
+			<span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+			<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+		  </a>
+		</div-->
+		<!-- /menu footer buttons -->
+	  </div>
+</div>
