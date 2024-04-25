2b160a3c0e583641befb06c0a821bf16fe1514da
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-04-25 18:42:51 +0530

    Add files via upload

---
+++ teacher/includes/left-nav.php
@@ -0,0 +1,84 @@
+<div class="col-md-3 left_col">
+	<div class="left_col scroll-view">
+		<div class="navbar nav_title" style="border: 0; background color= navy">
+		  <a href="index.php" class="site_title"><i class="fa fa-graduation-cap"></i> <span>School Management</span></a>
+        </div>
+        <div class="clearfix"></div>
+
+		<!-- sidebar menu -->
+		<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+		  <div class="menu_section">
+			<ul class="nav side-menu">
+			  <li><a href="index.php"><i class="fa fa-home"></i> Dashboard </a></li>
+			  <li><a><i class="fa fa-users"></i> Student<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="student-add.php">Add Student</a></li>
+					<li><a href="student-inform.php">Student Information</a></li>
+				</ul>
+			  </li>
+			  <li><a href="teacherlist.php"><i class="fa fa-user"></i> Teacher</a></li> 
+			  <li><a><i class="fa fa-book"></i> Subjects<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="class1.php">Class One</a></li>
+					<li><a href="class2.php">Class Two</a></li>
+					<li><a href="class3.php">Class Three</a></li>
+					<li><a href="class4.php">Class Four</a></li>
+					<li><a href="class5.php">Class Five</a></li>
+					<li><a href="class6.php">Class Six</a></li>
+					<li><a href="class7.php">Class Seven</a></li>
+					<li><a href="class8.php">Class Eight</a></li>
+				</ul>
+			  </li>
+			  <li><a><i class="fa fa-clock-o"></i> Class Routine<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="class1.php">Class One</a></li>
+					<li><a href="class2.php">Class Two</a></li>
+					<li><a href="class3.php">Class Three</a></li>
+					<li><a href="class4.php">Class Four</a></li>
+					<li><a href="class5.php">Class Five</a></li>
+					<li><a href="class6.php">Class Six</a></li>
+					<li><a href="class7.php">Class Seven</a></li>
+					<li><a href="class8.php">Class Eight</a></li>
+				</ul>
+			  </li>
+			  <li><a href="studymaterial.php"><i class="fa fa-clone"></i> Study Material </a></li>
+			  <li><a href="syllabus.php"><i class="fa fa-bars"></i> Academic Syllabus </a></li>
+			  <li><a><i class="fa fa-calendar"></i> Daily Attendance<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="attendance.php">Manage Attendance</a></li>
+				</ul>
+			  </li>
+			  <li><a><i class="fa fa-th-large"></i> Exam<span class="fa fa-chevron-down"></span></a>
+				<ul class="nav child_menu">
+					<li><a href="marks.php">Manage Marks</a></li>
+				</ul>
+			  </li>
+			  <li><a href="library.php"><i class="fa fa-th-list"></i> Library</a>
+			  <li><a href="transport.php"><i class="fa fa-bus"></i> Transport</a>  
+			  <li><a><i class="fa fa-circle"></i> Noticeboard</a> 
+			  <li><a><i class="fa fa-envelope"></i> Message</a>
+			  <li><a><i class="fa fa-laptop"></i> Account</a></li>
+			</ul>
+		  </div>
+		  
+
+		</div>
+
+		<!-- /menu footer buttons -->
+		<!--div class="sidebar-footer hidden-small">
+		  <a data-toggle="tooltip" data-placement="top" title="Settings">
+			<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+			<span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Lock">
+			<span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+		  </a>
+		  <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+			<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+		  </a>
+		</div-->
+		<!-- /menu footer buttons -->
+	  </div>
+</div>
